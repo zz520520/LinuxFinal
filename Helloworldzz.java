c058245fa7668b78cb300ed04d529fb83aa68881
zz520520 <841565698@qq.com>
2021-11-12 23:46:50 +0800

    1

---
+++ Helloworldzz.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class Helloworldzz extends HttpServlet {
+
+   public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String loginIp = request.getRemoteAddr();
+      out.println("<h1>hello world, " + loginIp + "</h1>");
+   }
+}
